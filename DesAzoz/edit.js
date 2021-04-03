6ae932b31d5cb08935d47d3739200cd46b34e80b
DevolperPHP <devolperphp@gmail.com>
2021-04-04 01:32:56 +0300

    Add files via upload

---
+++ DesAzoz/edit.js
@@ -0,0 +1,19 @@
+function color(){
+    var object = document.getElementById("text");
+    object.style.color = "red";
+}
+
+function size(){
+    var object = document.getElementById("text");
+    object.style.fontSize = "40px";
+}
+
+function font(){
+    var object = document.getElementById("text");
+    object.style.fontFamily = "Impact,Charcoal,sans-serif";
+}
+
+function remove(){
+    var object = document.getElementById("text");
+    object.style.display = "none";
+}
